5416f741c15a97f2f3260e300760df25fc22c6af
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-05-29 10:20:16 +0300

    init spring boot project

---
+++ src/main/java/itu/e3/newapp/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package itu.e3.newapp.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itu/e3/newapp/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package itu.e3.newapp.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
