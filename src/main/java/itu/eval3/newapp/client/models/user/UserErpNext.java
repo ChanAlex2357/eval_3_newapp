deac3c47553746a838e48f6de88a670870311317
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-05-31 14:57:52 +0300

    add forgor user role management

--- src/main/java/itu/eval3/newapp/client/models/user/UserErpNext.java
+++ src/main/java/itu/eval3/newapp/client/models/user/UserErpNext.java
@@ -6,21 +6,23 @@ import lombok.Data;
 public class UserErpNext {
 
     public static UserErpNext GUEST = new UserErpNext(null, null, null, "guest", null, null);
-
     private String sid;
     private UserKeys keys;
     private String username;
     private String email;
     private String fullName;
+    private UserRole[] roles ;
 
-    public UserErpNext(String sid, String apiKey, String apiSecret, 
-                     String username, String email, String fullName) {
+    public UserErpNext(String sid, String apiKey, String apiSecret, String username, String email, String fullName, UserRole[] roles) {
         this.sid = sid;
         this.keys = new UserKeys(apiKey, apiSecret);
         this.username = username;
         this.email = email;
         this.fullName = fullName;
-
+        this.roles = roles;
+    }
+    public UserErpNext(String sid, String apiKey, String apiSecret, String username, String email, String fullName){
+        this(sid, apiKey, apiSecret,username,email,fullName, new UserRole[0]);
     }
 
     public boolean isAuthenticated() {
@@ -32,5 +34,14 @@ public class UserErpNext {
             throw new SecurityException("User not authenticated "+keys+"  "+keys.isValid());
         }
         return "token " + getKeys().getAuthorizationKeys();
+    }
+
+    public boolean hasRole(String roleToCheck){
+        for (UserRole role : this.roles){
+            if (role.getRole().equals(roleToCheck)) {
+                return  true;
+            }
+        }
+        return  false;
     }
 }
