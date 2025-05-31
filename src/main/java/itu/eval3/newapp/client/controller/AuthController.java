e1d0ae55c64a43eb13bef87f6ccd7c27cbb30ed4
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-05-31 13:30:58 +0300

    base controller

--- src/main/java/itu/eval3/newapp/client/controller/AuthController.java
+++ src/main/java/itu/eval3/newapp/client/controller/AuthController.java
@@ -3,17 +3,19 @@ package itu.eval3.newapp.client.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 @RequestMapping("/auth")
-public class LoginController {
+public class AuthController {
     
     @GetMapping("/login")
     public String loginForm(){
         return "/auth/login";
     }
 
+    @PostMapping("login")
     public String doLogin(){
         return "redirect:/";
     }
