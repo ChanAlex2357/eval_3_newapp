25ca01f32798ebe6848fb9ca30ad93f743a223a7
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-06-01 16:12:08 +0300

    ajouter la liste des salaires dans les donnees pour le fiche de l'employé

--- src/main/java/itu/eval3/newapp/client/controller/EmpController.java
+++ src/main/java/itu/eval3/newapp/client/controller/EmpController.java
@@ -1,5 +1,7 @@
 package itu.eval3.newapp.client.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import itu.eval3.newapp.client.models.hr.emp.Employee;
+import itu.eval3.newapp.client.models.hr.salary.SalarySlip;
 import itu.eval3.newapp.client.models.user.UserErpNext;
 import itu.eval3.newapp.client.services.hr.emp.EmpService;
+import itu.eval3.newapp.client.services.hr.salary.SalarySlipService;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -17,6 +21,9 @@ import jakarta.servlet.http.HttpSession;
 public class EmpController {
     @Autowired
     private EmpService empService;
+
+    @Autowired
+    private SalarySlipService salarySlipService;
 
     @GetMapping
     public String list(HttpSession session,Model model){
@@ -26,18 +33,23 @@ public class EmpController {
     @GetMapping("/{id}")
     public String ficheEmployee(HttpSession session, Model model, @PathVariable String id) {
         Employee employee = null;
+        List<SalarySlip> salaries = null;
         UserErpNext user = (UserErpNext) session.getAttribute("user");
         try {
             employee = empService.getById(user, id);
 
-            if (employee == null ) {
-                model.addAttribute("error_msg", "Employee "+id+" est introuvable");
+            if (employee == null ) { // Erreur pour un employee inexistant
+                throw new Exception("Employee "+id+" est introuvable");
             }
+            
+            salaries = salarySlipService.getAllByEmployee(user, employee);
             
         } catch (Exception e) {
             model.addAttribute("error_msg", e.getMessage());
         }
+
         model.addAttribute("employee", employee);
+        model.addAttribute("salaries", salaries);
         return "hr/employee/fiche";
     }
 }
