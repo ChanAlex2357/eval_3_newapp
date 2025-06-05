9063dffd0c8e8fd162df7077555310b3ec833e7f
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-06-06 01:30:56 +0300

    login

--- src/main/java/itu/eval3/newapp/client/models/hr/salary/SalariesRegisterReport.java
+++ src/main/java/itu/eval3/newapp/client/models/hr/salary/SalariesRegisterReport.java
@@ -1,5 +1,7 @@
 package itu.eval3.newapp.client.models.hr.salary;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -8,4 +10,12 @@ import lombok.EqualsAndHashCode;
 public class SalariesRegisterReport {
     public SalarySlip[] salaries;
     public double totalAmount;
+    @JsonProperty("sum_earnings")
+    public double sumEarnings;
+
+    @JsonProperty("sum_deductions")
+    public double sumDeductions;
+
+    @JsonProperty("sum_salary")
+    public double sumSalary;
 }
