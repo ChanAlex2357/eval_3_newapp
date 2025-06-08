f5f2ec076b51ecad6aa7c22a49041c2e4b04cb31
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-06-08 07:44:01 +0300

    company service

---
+++ src/main/java/itu/eval3/newapp/client/models/hr/company/Company.java
@@ -0,0 +1,36 @@
+package itu.eval3.newapp.client.models.hr.company;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import itu.eval3.newapp.client.models.action.FrappeDocument;
+
+public class Company extends FrappeDocument {
+    @JsonProperty("company_name")
+    public String companyName;
+
+    @JsonProperty("abbr")
+    public String abbr;
+
+    @JsonProperty("default_currency")
+    public String defaulCurrency;
+
+    @JsonProperty("default_holiday_list")
+    public String defaulHolidayList;
+
+
+    @JsonProperty("country")
+    public String country;
+
+    public Company() {
+        super("Company");
+    }
+
+    @Override
+    public void update_cotnrole() {
+    }
+
+    @Override
+    public void save_controle() {
+    }
+    
+}
---
+++ src/main/java/itu/eval3/newapp/client/services/hr/company/CompanyService.java
@@ -0,0 +1,18 @@
+package itu.eval3.newapp.client.services.hr.company;
+
+import java.util.List;
+
+import itu.eval3.newapp.client.exceptions.ERPNexException;
+import itu.eval3.newapp.client.models.hr.company.Company;
+import itu.eval3.newapp.client.models.user.UserErpNext;
+import itu.eval3.newapp.client.services.frappe.FrappeCrudService;
+
+public class CompanyService extends FrappeCrudService<Company>{
+    private static Company document = new Company();
+    private static Class<Company> docClass = Company.class;    
+
+    public List<Company> getAll(UserErpNext user) throws ERPNexException{
+        List<Company> companies = getAllDocuments(user,document, null, null, docClass);
+        return companies;
+    }   
+}
